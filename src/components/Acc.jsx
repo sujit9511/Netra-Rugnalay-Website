25c3f1cef5fec199d5c1bd2372173b7762844c5c
mrmelody07 <115780774+mrmelody07@users.noreply.github.com>
2023-12-27 19:19:51 +0530

    descriptions

--- src/components/Acc.jsx
+++ src/components/Acc.jsx
@@ -5,27 +5,15 @@ function Acc() {
   return (
     <Accordion>
       <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Header>Retina surgery</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        Retina surgery is a delicate procedure performed to address conditions affecting the thin, light-sensitive tissue at the back of the eye. Surgeons use advanced techniques, such as vitrectomy, to remove or repair damaged areas in the retina. Precision is crucial to restore or enhance vision by addressing issues like retinal detachment, macular holes, or diabetic retinopathy. Microscopic instruments and laser technology may be employed to achieve optimal outcomes. Close post-operative monitoring is essential for successful recovery and improved visual function.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>Eye surgery</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        Eye surgery, also known as ocular surgery, involves procedures to treat various eye conditions. Surgeons use advanced techniques to correct refractive errors like myopia or astigmatism, remove cataracts, and address issues such as glaucoma. Procedures may include LASIK for vision correction or cataract surgery to replace a cloudy lens with an artificial one. Surgeons prioritize precision and safety, often utilizing cutting-edge technology like laser systems for optimal outcomes. Recovery times vary, but many patients experience improved vision shortly after the procedure.
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
